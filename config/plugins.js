66e22bf0d912b94f62d792b76e0b84a6ee93a826
Aleksandr Klimenkov <alx.devel@gmail.com>
2022-01-17 20:31:41 +0300

    amazon s3

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  upload: {
+    enabled: true,
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+    },
+  },
+});
